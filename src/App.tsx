df60bf70224171a25223cd341991175e6babfadf
Bernardo Estrada <49007495+BernardoEstrada@users.noreply.github.com>
2023-09-07 12:52:34 -0600

    Move changing theme logic to nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
-import { useEffect, useState } from 'react'
-import Themefy, { DEFAULT_DARK, DEFAULT_LIGHT, Themes, getThemeColors } from './utils/Theme'
+import { useState } from 'react'
+import Themefy, { DEFAULT_DARK, getThemeColors } from './utils/Theme'
 import resume from '@assets/BernardoEstrada.resume.json' assert { type: "json" }
 import BubbleChart from './components/BubbleChart'
 import NavBar from './components/NavBar'
 import Landing from './components/Landing'
 import { Helmet } from 'react-helmet'
-import usePrefersColorScheme from 'use-prefers-color-scheme'
-import faviconDark from '@assets/favicon-dark.svg' assert { type: "svg" }
-import faviconLight from '@assets/favicon-light.svg' assert { type: "svg" }
 
 console.log('%c Hello There!', 'font-size: 36px; font-weight: bold');
 function App() {
-  const preferredScheme = usePrefersColorScheme();
-  const preferredTheme = localStorage.getItem('theme') as Themes || Themes.DEVICE_SETTINGS
-  const [theme, setTheme] = useState(preferredTheme)
+  const [theme, setTheme] = useState(DEFAULT_DARK)
   const [colors, setColors] = useState(getThemeColors())
-
-  useEffect(() => {
-    localStorage.setItem('theme', theme)
-    if (theme === Themes.DEVICE_SETTINGS) setTheme(preferredScheme === 'dark' ? DEFAULT_DARK : DEFAULT_LIGHT)
-    setTimeout(() => setColors(getThemeColors()), 0)
-  }, [theme])
-  useEffect(() => {
-    if (theme === DEFAULT_DARK && preferredScheme !== 'dark') setTheme(DEFAULT_LIGHT)
-    if (theme === DEFAULT_LIGHT && preferredScheme === 'dark') setTheme(DEFAULT_DARK)
-  }, [preferredScheme])
-
-  const getIcon = (schema: "dark" | "light" | "no-preference") => {
-    if (schema === 'dark') return faviconDark
-    if (schema === 'light') return faviconLight
-    return theme === DEFAULT_DARK ? faviconDark : faviconLight
-  }
 
   return (
     <>
@@ -38,12 +17,12 @@ function App() {
       <Helmet >
         <title>{resume.basics.name}</title>
         <meta name="description" content={resume.basics.headline} />
-        <link rel="icon" type="image/svg+xml" href={getIcon(preferredScheme)} />
+        <meta name="theme-color" content={colors.b1} />
       </Helmet>
-      <NavBar theme={theme} setTheme={setTheme} />
+      <NavBar theme={theme} setTheme={setTheme} setColors={setColors} />
 
       <Landing />
-      <div className="hidden">
+      <div className="">
         {/* todo */}
         <br/>
         <span className="text-blue-500 ">{resume.basics.summary}</span>
