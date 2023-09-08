a79647f6b6db853cc80f7ddef356c329567d052a
Bernardo Estrada <49007495+BernardoEstrada@users.noreply.github.com>
2023-09-08 16:22:27 -0600

    move wipcard to comp

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -12,7 +12,7 @@ import headshot from "@assets/headshot.jpg" assert { type: "jpg" }
 import HelloWorld from "@assets/HelloWorld.json" assert { type: "json" }
 import { TypeAnimation } from "react-type-animation";
 import * as r_ from "radash";
-import pdfResume from "@assets/BernardoEstrada.resume.pdf" assert { type: "pdf" }
+import WIPCard from "./WIP.card";
 
 export default function Landing() {
   const particlesInit = useCallback(async (engine: Engine) => {
@@ -79,38 +79,9 @@ export default function Landing() {
             </p>
           </div>
         </div>
-
-        <div className="card grid grid-rows-1 md:grid-cols-3 justify-items-center items-center max-w-fit max-h-fit shadow-2xl bg-base-100 p-12 mt-16 md:mt-8 mb-16 md:mb-0">
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-          <div className="md:col-span-3">
-            <div className="flex flex-col justify-start space-x-4 max-w-2xl">
-              <h3 className="text-2xl font-bold text-left text-primary-focus">
-                Work in progress
-              </h3>
-              <br/>
-              <p className="text-2xl text-left text-base-content">
-                I'm currently working on my portfolio, check back soon!
-                Meanwhile you can check out my GitHub, LinkedIn or resume.
-              </p>
-              <p className="text-5xl text-center text-base-content mt-3">
-                <a href="https://github.com/BernardoEstrada" target="_blank">
-                  <i className="devicon-github-original mx-4"></i>
-                </a>
-                <a href="https://linkedin.com/in/bernardoef/" target="_blank">
-                  <i className="devicon-linkedin-plain mx-4"></i>
-                </a>
-                <a href={pdfResume} target="_blank" className="mx-4 my-0 align-text-top">
-                    <svg className="inline-block w-12 h-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                    </svg>
-                </a>
-
-              </p>
-          
-          
-
-            </div>
-          </div>
+        <div className="h-64 md:h-8"></div>
+        <div className="mb-24 md:mb-0">
+          <WIPCard />
         </div>
       </div>
   );
